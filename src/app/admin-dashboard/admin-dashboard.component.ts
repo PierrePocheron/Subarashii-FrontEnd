3149561ae6fd2689c0348f803dce9b540fd599a3
JulieMlr <julie.miler.jm@gmail.com>
2022-05-31 11:27:52 +0200

    [ADD] infos dashboard admin

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, ChartItem, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
+  nbUser: number;
+  nbAdmin: number;
 
-  constructor() { }
+  constructor() {
+    this.nbUser = 10;
+    this.nbAdmin = 3
+  }
 
   ngOnInit(): void {
+    const ctx = document.getElementById('myChart') as ChartItem;
+    const myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ['User', 'Admin'],
+        datasets: [{
+          data: [this.nbUser, this.nbAdmin],
+          backgroundColor: [
+            'rgb(255,71,63)',
+            'rgb(194,0,27)',
+          ],
+        }]
+      },
+
+    });
   }
 
 }
